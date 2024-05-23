ff90e457ff2a669d4fb41cd828c15eba70c8f63f
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-05-23 23:37:14 +0300

    Добавлены конфигурации предусловий

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,11 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -11,6 +14,12 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @BeforeEach
+    void logger(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterEach
