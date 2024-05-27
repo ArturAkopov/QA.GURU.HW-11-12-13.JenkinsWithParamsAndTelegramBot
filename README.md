7adf760ea4eedd995a5e11a2e1465db9e4445c88
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-05-27 23:35:29 +0300

    Добавил ввод переменных из Jenkins

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # QA.GURU.HW-11.Jenkins
 ## 11.Jenkins
-### Домашнее задание №7
+### Домашнее задание №11
 
 1) Взять свой код для http://demoqa.com/automation-practice-form (https://github.com/ArturAkopov/QA.GURU.HW-7.GeneratingTestData/tree/main)
 2) Добавить аттачи для Allure – скриншот, page source, console.log и видео
 3) Cделать сборку в Jenkins
+
+## 12.Jenkins with params
+### Домашнее задание №12
+
+Доработать свой код:
+1) Передать из дженкинса адрес удаленного браузера (selenoid)
+2) Сделать возможность выбора браузера, версии и разрешения из сборки дженкинс
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,9 +17,12 @@ public class TestBase {
     static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("size", "1920x1080");
+        Configuration.browserVersion = System.getProperty("version","127");
+
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@"+System.getProperty("selenoid","selenoid.autotests.cloud")+"/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
